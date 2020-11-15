4f3b5803f1d3eef37bfd22beddd10dcb64fba9ab
YaninaBankova <43926387+YaninaBankova@users.noreply.github.com>
2020-11-15 15:49:17 +0200

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,51 @@
+package edu.smg;
+
+public class Time {
+	private int hour;
+	private int minute;
+	private int second;
+	
+	Time(){
+		setTime(System.currentTimeMillis ());
+	}
+	
+	Time(long milliseconds){
+		setTime(milliseconds);
+	}
+	
+	Time( int hour, int minute, int second){
+		setHour(hour);
+		setMinute(minute);
+		setSecond(second);
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+	public int getSecond() {
+		return second;
+	}
+
+	public void setSecond(int second) {
+		this.second = second;
+	}
+	
+	public void setTime(long elapsedTime) {
+		setHour((int)(elapsedTime / (1000 * 60 * 60)) % 24);
+		setMinute((int)(elapsedTime / (1000 * 60)) % 60);
+		setSecond((int)(elapsedTime / 1000) % 60);
+	}
+}
